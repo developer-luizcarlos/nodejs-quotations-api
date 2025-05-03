088bf0d225b0e4f26a032a656d0c812ced3d2fa1
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-03 17:12:20 -0300

    refact: fix errors when the main route creation

--- api/main.ts
+++ api/main.ts
@@ -1,6 +1,5 @@
 import app from "../src/main.js";
-import { createServer } from "http";
-import { ServerResponse, IncomingMessage } from "http";
+import { createServer, ServerResponse, IncomingMessage } from "http";
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   const server = createServer(app);
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { appRouter } from "./routes/quotes.routes";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/quotes", appRouter);
+app.use("/", appRouter);
 
 export default app;
